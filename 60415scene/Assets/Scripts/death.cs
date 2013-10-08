09ad92aa5c31c6df786a8f9b4d2fc5ab50e6e6e1
Jason Israel <jason.a.israel@gmail.com>
2013-10-07 21:30:19 -0700

    first build completed

---
+++ 60415scene/Assets/Scripts/death.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class death : MonoBehaviour {
+
+	public Vector3 spawnpos;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter( Collider other) {
+		Debug.Log(other.gameObject.name);
+		if (other.gameObject.name == "First Person Controller") {
+			other.gameObject.transform.Translate(spawnpos - other.gameObject.transform.position);
+			Debug.Log(other.gameObject.transform.position);
+		}
+	}
+}
--- 60415scene/Assets/Scripts/globalVars.cs
+++ 60415scene/Assets/Scripts/globalVars.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class globalVars : MonoBehaviour {
 
-	public bool enabled = false;
 
 	// Use this for initialization
 	void Start () {
