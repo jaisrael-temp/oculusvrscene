5bd5fefc240c5b41e69b0c2adce8c63d9c97ffad
jaisrael <jasonisrael@cmu-856858.wv.cc.cmu.edu>
2013-10-10 11:13:44 -0400

    potential cutscene created

--- 60415scene/Assets/Scripts/enablePlatform.cs
+++ 60415scene/Assets/Scripts/enablePlatform.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class enablePlatform : MonoBehaviour {
 
+	//var cutsceneDelay = 2;
+	//var nextUsage;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -12,11 +15,12 @@ public class enablePlatform : MonoBehaviour {
 	void Update () {
 	
 	}
-	void OnTriggerEnter( Collider other) {
-		
+	void OnTriggerEnter (Collider other) {
 		foreach (var plat in GameObject.FindGameObjectsWithTag("platform")) {
 			var p = plat.GetComponent("simpleRepeatedLinear") ?? plat.GetComponent("generalLooping");
 			(p as platform).movementEnabled = true;
 		}
+		GameObject.Find("Main Camera").camera.active = false;
+		GameObject.Find("PlatfromEnabledCamera").camera.active = true;
 	}
 }
