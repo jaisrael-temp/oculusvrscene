9e7fbfb25f89b0efe31232f3577492be82957cad
Jason Israel <jason.a.israel@gmail.com>
2013-11-07 03:33:14 -0800

    basic trigger code complete

---
+++ 60415scene/Assets/Scripts/triggerHandler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class triggerHandler : MonoBehaviour {
+
+	public enum Triggers {firstGap}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void handleTrigger (Triggers t) {
+
+		switch (t) {
+			case Triggers.firstGap:
+				var platform = GameObject.Find("gap_platform_1");
+				platform.renderer.enabled = true;
+				Debug.Log(platform);
+				break;
+			default:
+				break;
+		}
+	}
+}
---
+++ 60415scene/Assets/Scripts/triggerPlane.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class triggerPlane : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider c) {
+		var handler = (GameObject.Find("First Person Controller").GetComponent("triggerHandler") as triggerHandler);
+		handler.handleTrigger(triggerHandler.Triggers.firstGap);
+	}
+}
