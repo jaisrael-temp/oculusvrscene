24d693db918a709a65744cca51ea13497c8f1055
jaisrael <jasonisrael@cmu-856858.wv.cc.cmu.edu>
2013-11-14 09:49:13 -0500

    cloud animation possible

---
+++ 60415scene/Assets/Scripts/fluctuate.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fluctuate : MonoBehaviour {
+
+	private int frame = 0;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		frame++;
+		if (frame > 100) {
+			var emitter = (gameObject.GetComponents(typeof(ParticleEmitter))[0] as ParticleEmitter);
+			var c = emitter.particles;
+			for(int i = 0; i < c.Length; i++) {
+				c[i].position += new Vector3(0.0f, 0.1f, 0.0f);
+				if(i == 0) {
+					Debug.Log(c[i].position);
+					Debug.Log(frame);
+				}
+			}
+			emitter.particles = c;
+
+		}
+	}
+}
